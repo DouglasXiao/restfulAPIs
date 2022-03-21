06a13f5154f56e793170ca27244fbbd08759192e
Douglas <douglas@nikas-MacBook-Pro.local>
2022-03-20 20:00:58 -0700

    updates
    body function

---
+++ app.js
@@ -0,0 +1,32 @@
+const http = require('express');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http();
+server.use(http.json());
+server.use(http.urlencoded({ extended: true }));
+
+server.listen(port, () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+server.get("/gettest/:version", (req, res, next) => {
+        res.send(req.params.version);
+});
+
+server.post("/posttest/:id", (req, res) => {
+        const { id } = req.params;
+        const { title, book } = req.body;
+        res.json(`This is a POST request and you input ${id} in the query parameter, also your title is: ${title}, you bought a book called: ${book}`);
+});
+
+server.put("/puttest/:id", (req, res) => {
+        const { id } = req.params;
+        res.json(`This is a PUT request and you input ${id} in the query parameter`);
+});
+
+server.delete("/deletetest/:id", (req, res) => {
+        const { id } = req.params;
+        res.send({ 'id': id});
+});
